d2d2a98f5e0c559dbd050a1d3a2c5c639c063f35
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-14 11:55:38 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     //let taskDescription = e.target.new-task-description.value;
-    taskHandler(e.target.new-task-description.value);
+    
+    console.log(e);
+    taskHandler(e.target[0].value);
   })
 
   function taskHandler(toDo){
     let p = document.createElement('p');
-    p.textContent = toDo;
+    let btn = document.createElement('button');
+    btn.addEventListener('click', (e) => {
+      e.target.parentNode.remove();
+    })
+    p.textContent = `${toDo}   `;
+    btn.textContent = 'x';
+    p.appendChild(btn);
     document.getElementById('tasks').appendChild(p);
   }
-
 })
 
