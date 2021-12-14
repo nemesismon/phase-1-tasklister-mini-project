9c8155f98ee684d78e9f3700fcf120b16c9fc7ec
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-14 11:26:48 -0500

    TC

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  //console.log(document.querySelector('form'));
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    //let taskDescription = e.target.new-task-description.value;
+    taskHandler(e.target.new-task-description.value);
+  })
+
+  function taskHandler(toDo){
+    let p = document.createElement('p');
+    p.textContent = toDo;
+    document.getElementById('tasks').appendChild(p);
+  }
+
+})
+
